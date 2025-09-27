72046eaba8725c7a46e1f3a5c23b0add3f7f56b9
Prataya <pratayasilla@gmail.com>
2025-09-27 07:57:37 +0530

    Change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="">
       <div className="text-center space-y-4">
-        <h1 className="text-4xl font-bold">Welcome to VibeCode</h1>
+        <h1 className="text-4xl font-bold">Let's VibeCode</h1>
         </div>
     </div>
   );
